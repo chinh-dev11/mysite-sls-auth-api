e0a58ce8b310aabaf25cbfe12514e642cb85cb2a
Chinh Le <chinh.le11@gmail.com>
2020-05-05 09:35:33 -0400

    Initial commit

---
+++ home.js
@@ -0,0 +1,11 @@
+exports.main = async (event) => ({
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    },
+    null,
+    2,
+  ),
+});
